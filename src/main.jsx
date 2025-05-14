d4897e1f2edabdf20d3b25795f4069edaf4c9705
krishian07 <krisgian07@gmail.com>
2025-05-14 16:43:22 -0500

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import GananciaCalculadora from './GananciaCalculadora'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GananciaCalculadora />
+  </React.StrictMode>
+)
